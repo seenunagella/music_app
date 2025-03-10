38fdbd12818b5dd1b0c8e3cf570a85de7d9fb2fa
seenunagella <srinuharshi12@gmail.com>
2025-03-10 12:07:06 +0530

    first commit

---
+++ Spotipy.py
@@ -0,0 +1,196 @@
+import spotipy
+import spotipy.oauth2 as oauth2
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time
+import json
+'''
+Store the client id and secret in a config.json file:
+{
+  "spotify_client_id": "your_client_id",
+  "spotify_client_secret": "your_client_secret"
+}
+'''
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+client_id = config['11173cae9ac949d38538e1cfa0aaab94']
+client_secret = config['35a74caecfbb42f28a351ee32ef1eb17']
+
+auth_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def getTrackIDs(user, playlist_id):
+    track_ids = []
+    playlist = sp.user_playlist(user, playlist_id)
+    for item in playlist["tracks"]["items"]:
+        track = item["track"]
+        track_ids.append(track["id"])
+    return track_ids
+
+
+def getTrackFeatures(id):
+    track_info = sp.track(id)
+
+    name = track_info["name"] or "null"
+    album = track_info["album"]["name"] or "null"
+    artist = track_info["album"]["artists"][0]["name"] or "null"
+    # release_date = track_info['album']['release_date']
+    # length = track_info['duration_ms']
+    # popularity = track_info['popularity']
+    image = track_info['album']['images'][0]['url'] or "null"
+    preview_url = track_info["preview_url"] or "null"
+    
+    track_data = [name, album, artist,preview_url,image]  # , release_date, length, popularity
+    return track_data
+
+
+# Code for creating dataframe of feteched playlist
+
+emotion_dict = {
+    0: "Angry",
+    1: "Disgusted",
+    2: "Fearful",
+    3: "Happy",
+    4: "Neutral",
+    5: "Sad",
+    6: "Surprised",
+}
+music_dist = {
+    0: "289niu87jQv6wijmNnKE0L",
+    1: "3qgzMg4m5tvf16PzlPgGa9",
+    2: "3W6P20WXYSNACvkGnGabEE",
+    3: "37i9dQZF1DXdPec7aLTmlC",
+    4: "4kvSlabrnfRCQWfN0MgtgA?si=b36add73b4a74b3a",
+    5: "3Ar6l24242VBGny7S9VxcD",
+    6: "51Ca05x9QkTaA63dg4NWyP",
+}
+
+"""
+Code can def be modularised into a function but i tried to write it when i was extremely sleepy so thought screw it and repeated code block
+
+Uncomment for fetching updated playlists
+"""
+
+
+track_ids = getTrackIDs("spotify", music_dist[0])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/angry.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[1])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/disgusted.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[2])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/fearful.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[3])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/happy.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[4])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/neutral.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[5])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/sad.csv")
+print("CSV Generated")
+
+track_ids = getTrackIDs("spotify", music_dist[6])
+track_list = []
+for i in range(len(track_ids)):
+    time.sleep(0.3)
+    track_data = getTrackFeatures(track_ids[i])
+    is_null = False
+    for el in track_data:
+        if el == "null":
+            is_null = True
+            break
+    if not is_null:
+        track_list.append(track_data)
+    df = pd.DataFrame(
+        track_list, columns=["Name", "Album", "Artist", "Link", "Image"]
+    )  # ,'Release_date','Length','Popularity'
+    df.to_csv("songs/surprised.csv")
+print("CSV Generated")
---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, Response, jsonify, request
+import gunicorn
+from camera import *
+from PIL import Image
+
+app = Flask(__name__)
+
+headings = ("Name", "Album", "Artist")
+df1 = music_rec()
+df1 = df1.head(15)
+
+
+@app.route("/")
+def index():
+    print(df1.to_json(orient="records"))
+    return render_template("index.html", headings=headings, data=df1)
+
+@app.route('/app')
+def app_page():
+    # print(df1.to_json(orient="records"))
+    return render_template("app.html")
+
+
+def gen(camera):
+    while True:
+        global df1
+        frame, df1 = camera.get_frame()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(
+        gen(VideoCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+@app.route("/live_emotion")
+def live_emotion():
+    return real_time_emotion()
+
+@app.route("/t")
+def gen_table():
+    print(df1)
+    return df1.to_json(orient="records")
+
+@app.route("/get_recommendations")
+def get_recommendations():
+    [emotion,df1] = max_emotion_reccomendation()
+    return jsonify({"detected_emotion":emotion,"music_data":df1.to_dict(orient="records")if df1 is not None else None})
+
+@app.route('/image', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file part in the request', 400
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return 'No selected file', 400
+    if file and allowed_file(file.filename):
+        image = Image.open(file.stream)
+        image_array = np.array(image)
+        [picture,detected_emotion]=emotion_rec(image_array)
+        return jsonify({"emotion": detected_emotion})
+
+
+def allowed_file(filename):
+    # Check if file extension is allowed
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif'}
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=6969)
